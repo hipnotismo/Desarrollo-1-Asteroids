b7a8dfac034679c71b48be616df912a05ca608f7
hipnotismo <martinconcetti97@gmail.com>
2018-10-02 09:41:53 -0300

    Se agrego carpeta de proyecto

---
+++ Asteroids/Asteroids/Juego.h
@@ -0,0 +1,10 @@
+#ifndef Juego_H
+#define Juego_H
+
+ void InitGame(void);         
+ void UpdateGame(void);       
+ void DrawGame(void);         
+ void UnloadGame(void);      
+ void UpdateDrawFrame(void);
+
+#endif // !Juego_H
